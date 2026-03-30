612f46bccc1674b12be592751e15e7d54d4315e9
Lucas Kluska Donini <lucas.donini@picpay.com>
2026-03-30 17:26:10 -0300

    feat(response): add /cep endpoint response type

---
+++ src/model/Address.py
@@ -0,0 +1,36 @@
+from pydantic import BaseModel
+from model.external.CepApiSuccess import CepApiSuccess
+from model.Cep import Cep
+
+
+class Address(BaseModel):
+    cep: Cep
+    state: str
+    city: str
+    neighborhood: str
+    street: str
+    coordinates: str
+
+    @classmethod
+    def from_response(cls, response: CepApiSuccess) -> "Address":
+        cep: Cep = Cep(value=response.cep)
+
+        latitude: float = float(response.location.coordinates.latitude)
+        is_south: bool = latitude < 0
+
+        longitude: float = float(response.location.coordinates.longitude)
+        is_west: bool = longitude < 0
+
+        coordinate: str = (
+            f"{abs(latitude)}°{'S' if is_south else 'N'} "
+            f"{abs(longitude)}°{'O' if is_west else 'L'}"
+        )
+
+        return Address(
+            cep=cep,
+            state=response.state,
+            city=response.city,
+            neighborhood=response.neighborhood,
+            street=response.street,
+            coordinates=coordinate,
+        )
---
+++ tests/model/test_Address.py
@@ -0,0 +1,24 @@
+from model.Address import Address
+from model.external.CepApiSuccess import CepApiSuccess, _Location, _Coordinate
+
+
+def test_valid_data():
+    response: CepApiSuccess = CepApiSuccess(
+        cep="00000000",
+        state="SP",
+        city="São Paulo",
+        neighborhood="Vila Leopoldina",
+        street="Rua exemplo",
+        location=_Location(
+            type="Point", coordinates=_Coordinate(latitude="-20", longitude="-20")
+        ),
+    )
+
+    address: Address = Address.from_response(response)
+
+    assert address.cep.value == response.cep
+    assert address.state == response.state
+    assert address.city == response.city
+    assert address.neighborhood == response.neighborhood
+    assert address.street == response.street
+    assert address.coordinates == "20.0°S 20.0°O"
