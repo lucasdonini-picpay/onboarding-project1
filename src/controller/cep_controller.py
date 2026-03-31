6bbf31273c57431ee95d227977feab2215ab45c5
Lucas Kluska Donini <lucas.donini@picpay.com>
2026-03-31 15:37:56 -0300

    feat(middleware): add exception handling middleware

--- src/controller/cep_controller.py
+++ src/controller/cep_controller.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
+from fastapi.responses import JSONResponse
 from dotenv import load_dotenv
+from httpx import Response
 import httpx, os
 
 from src.model.external.CepApiSuccess import CepApiSuccess
@@ -12,16 +14,20 @@ router = APIRouter()
 
 
 @router.get("/cep/{cep}")
-async def get_cep(cep: str):
+async def get_cep(cep: str) -> JSONResponse:
     if not BASE_URL:
         raise RuntimeError("Missing environment variable: BASE_URL")
 
     if not validate_cep(cep):
         raise ValueError("Invalid CEP")
 
+    response: Response
     client = httpx.AsyncClient(verify=False)
     response = await client.get(f"{BASE_URL}/{cep}")
-    response.raise_for_status()
-    parsed_response = CepApiSuccess(**response.json())
-    address = Address.from_response(parsed_response)
-    return address
+    try:
+        response.raise_for_status()
+        parsed_response = CepApiSuccess(**response.json())
+        address = Address.from_response(parsed_response)
+        return JSONResponse(status_code=200, content=address)
+    except Exception:
+        return JSONResponse(status_code=response.status_code, content=response.json())
--- src/main.py
+++ src/main.py
@@ -1,8 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 from .controller import cep_controller
+import logging
 
 app = FastAPI()
 app.include_router(cep_controller.router)
+
+
+@app.middleware("http")
+async def exception_handler(request: Request, call_next):
+    try:
+        response = await call_next(request)
+        return response
+    except Exception as e:
+        logging.error(f"Exception captured by middleware: {e}")
+        match e:
+            case ValueError():
+                return JSONResponse(status_code=400, content={"detail": f"{e}"})
+            case _:
+                return JSONResponse(
+                    status_code=500,
+                    content={
+                        "detail": "Something went wrong. Please, try again later."
+                    },
+                )
 
 
 @app.get("/test")
